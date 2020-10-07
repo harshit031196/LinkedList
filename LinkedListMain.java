660cc5fbf387c3cb36aed6ca101194c9c387c2d4
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-08 01:57:07 +0530

    This is the main file

---
+++ LinkedListMain.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList l = new LinkedList();
+		LinkedList.insertNode(l, 56);
+		LinkedList.insertNode(l, 30);
+		LinkedList.insertNode(l, 70);
+		LinkedList.printLinkedList(l);
+	}
+
+}
